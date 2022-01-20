aed02389daab39a037c5d3e523a69535c7067743
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-20 15:51:59 -0500

    Refactored to use callbacks.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,30 @@
-let breed = process.argv.slice(2);
+// let breed = process.argv.slice(2);
 
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed[0]}`, (error, response, body, search) => {
-  if (response.statusCode !== 200) {
-    throw error;
-  } else {
-    let data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log("Breed cannot be found.");
-    } else {
-      console.log(data[0].description);
+const fetchBreedDescription = function(breedName, callback) {
+  const url = (`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`);
+  request(url, (error, response, body) => {
+    if (error) {
+    //send back the error
+      return callback(error, null);
     }
-  }
-});
+    let data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description)
+    } else {
+      callback ('breed not found');
+    }
+    // if (response.statusCode !== 200) {
+    //   return callback(`Server response: ${response.statusCode}`, null);
+    // }
+    // if (data[0] === undefined) {
+    //   console.log("Breed cannot be found.");
+    // }
+      // return callback(null, data[0].description);
+  })
+}; 
+
+module.exports = { fetchBreedDescription };
 
